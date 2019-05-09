a003d51ed0a537a56d8c4a76627d85cc92bd9586
matthew <mattmarelli@gmail.com>
2019-05-09 09:36:44 -0700

    can now 'switch' between holes.  need to read in datafiles now and get gps locations to do distance calculations

--- newcastleGolfCourseGPS/components/DistanceDisplay.js
+++ newcastleGolfCourseGPS/components/DistanceDisplay.js
@@ -4,11 +4,20 @@ import console from 'console';
 import { file } from '@babel/types';
 
 class DistanceDisplay extends Component {
-        state = {
+    constructor(props){
+        super(props)
+        this.state = {
+            currentHole: 1,
             courseName: this.props.navigation.getParam('courseName'),
             gpsData: null,
-        };
-        // var fileName = '../holeData/' + String(this.state.courseName) + '.csv'
+            distanceToFront: 1,
+            distanceToMiddle: 1,
+            distanceToBack: 1,
+        }
+        this.updateDistance = this.updateDistance.bind(this);
+    }
+
+    // var fileName = '../holeData/' + String(this.state.courseName) + '.csv'
     // componentDidMount() {
     //     var fileName = '../holeData/' + String(this.props.navigation.getParam('courseName')) + '.csv'
     //     var reader = new FileReader();
@@ -16,6 +25,16 @@ class DistanceDisplay extends Component {
     //     console.log(data)
     // }
 
+    // method is not correct need to change once I get the gps locations and hole locations
+    updateDistance(currentHole, nextUse) {
+        this.setState({
+            distanceToBack: currentHole + nextUse,
+            distanceToFront: currentHole + nextUse,
+            distanceToMiddle: currentHole + nextUse,
+        })
+    }
+
+    
     render() {
         var fileName = '../holeData/' + String(this.props.navigation.getParam('courseName')) + '.csv'
         var myBlob = new Blob()
@@ -25,9 +44,40 @@ class DistanceDisplay extends Component {
         return (
             <View>
                 <Text>
-                    You selected {this.state.courseName}
-                    {fileName}
+                    Hole {this.state.currentHole}
                 </Text>
+                <Text>
+                    You selected {this.state.courseName} 
+                    distance to front {this.state.distanceToFront} 
+                    distance to middle {this.state.distanceToMiddle} 
+                    distance to back {this.state.distanceToBack}
+                </Text>
+                <TouchableOpacity
+                    onPress={()=>{
+                            if (this.state.currentHole < 18) {
+                                this.setState({
+                                    currentHole: this.state.currentHole + 1
+                                });
+                                this.updateDistance(this.state.currentHole, 1);
+                            }
+                        }}>
+                    <Text>
+                        Next Hole
+                    </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={()=>{
+                            if (this.state.currentHole > 1) {
+                                this.setState({
+                                    currentHole: this.state.currentHole - 1
+                                });
+                                this.updateDistance(this.state.currentHole, -1);
+                            }
+                        }}>
+                    <Text>
+                        Previous Hole
+                    </Text>
+                </TouchableOpacity>
             </View>
         )
     }
